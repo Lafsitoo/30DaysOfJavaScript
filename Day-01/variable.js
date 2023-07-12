eaa765382a183c60a4c92da85ea8c94fa1ca4d71
Lafsitoo <fretteleandro@live.com>
2023-07-12 13:28:24 -0300

    first commit

---
+++ Day-01/variable.js
@@ -0,0 +1,13 @@
+// Los comentarios pueden hacer que el código sea legible
+// Bienvenido a 30DaysOfJavaScript
+/* Los comentarios pueden hacer el código legible, fácil de usar e informativo */
+
+const isString = "Hola Mundo"
+const isBoolean = true
+const isUndefined = undefined
+const isNull = null
+
+typeof isString === "string"
+typeof isBoolean === "boolean"
+typeof isUndefined === "undefined"
+typeof isNull === "object"
